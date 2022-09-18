4b5c5230dcd736b10e03d926df85d257c8f69e63
kayslaouar <1kayslaouar@gmail.com>
2022-09-18 13:19:30 -0400

    created file for error messages

---
+++ errorMessages.js
@@ -0,0 +1,7 @@
+export default errorMessages = {
+	missingInfo: 'Missing required information',
+	userDNE: 'User does not exist',
+	postDNE: 'Post does not exist',
+	commentDNE: 'Comment does not exist',
+	internalServer: 'Internal server error',
+};
